c0f963ca85e11453e5306f2162585b30b5ebcfd4
garage@conservationxlabs.org <garage@conservationxlabs.org>
2019-07-31 14:50:59 -0400

    Updated Master Code

--- find_face.py
+++ find_face.py
@@ -14,11 +14,11 @@ anchor = (1.889, 2.5245, 2.9465, 3.94056, 3.99987, 5.3658, 5.155437, 6.92275, 6.
 a = kpu.init_yolo2(task, 0.5, 0.3, 5, anchor)
 while(True):
     img = sensor.snapshot()
+    # check if this works: img.save(home/Desktop/CameraTrapImages/img.png)
     code = kpu.run_yolo2(task, img)
     if code:
         for i in code:
-            # print(i)
             a = img.draw_rectangle(i.rect())
-            return True 
+            print(True)
     a = lcd.display(img)
 a = kpu.deinit(task)
