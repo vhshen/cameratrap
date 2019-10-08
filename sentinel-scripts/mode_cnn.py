8c885294c17e33b263fe5134c9ab3051067477ab
Sam Kelly <samkelly5941@gmail.com>
2019-10-08 11:56:45 -0400

    Add

--- sentinel-scripts/mode_cnn.py
+++ sentinel-scripts/mode_cnn.py
@@ -215,17 +215,17 @@ def cnn(sys_mode, mcu, format, type, resolution, \
         data_directory = os.path.join('../',data_directory)
         results_directory = os.path.join('../',results_directory)
 
-    if reset_results == 1:
-        import os, shutil
-        folder = results_directory
-        for the_file in os.listdir(folder):
-            file_path = os.path.join(folder, the_file)
-            try:
-                if os.path.isfile(file_path):
-                    os.unlink(file_path)
-            #elif os.path.isdir(file_path): shutil.rmtree(file_path)
-            except Exception as e:
-                print(e)
+    #if reset_results == 1:
+    #    import os, shutil
+    #    folder = results_directory
+    #    for the_file in os.listdir(folder):
+    #        file_path = os.path.join(folder, the_file)
+    #        try:
+    #            if os.path.isfile(file_path):
+    #                os.unlink(file_path)
+    #        #elif os.path.isdir(file_path): shutil.rmtree(file_path)
+    #        except Exception as e:
+    #            print(e)
 
     #print('Loaded CNN Parameters')
 
